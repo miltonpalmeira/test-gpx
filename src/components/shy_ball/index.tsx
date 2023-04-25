a89509e265b941383ca02d652a449381283abf79
Milton Palmeira <lpsantana_@hotmail.com>
2023-04-25 16:27:50 -0300

    Revert "add(clock-item-handler): final adjustes to the item handler - added comments"
    This reverts commit 8a575d7e59d910c5fa6e49ab3f975cec56549413.

--- src/components/shy_ball/index.tsx
+++ src/components/shy_ball/index.tsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
 import styles from './shy_ball.module.css';
 
 export default function ShyBall() {
-  const [positionX, setPositionX] = useState(0);
-  const [positionY, setPositionY] = useState(0);
-  
-  const moveBall = () => {
-    const box = document.querySelector('#container');
-    setPositionX(Math.floor(Math.random() * 10) * (box!.clientHeight / 10));
-    setPositionY(Math.floor(Math.random() * 10) * (box!.clientHeight / 10) - 50);
-  };
-
-  return (
-    <div className={styles.container} id='container'>
-      <div
-        className={styles.ball}
-        id='ball'
-        style={{
-          transform: `translate(${positionX}px, ${positionY}px)`,
-        }}
-        onMouseOver={moveBall}
-      ></div>
-    </div>
-  );
+  return <div className={styles.background}>Shy ball</div>;
 }
